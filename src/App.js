b2432116c60a253dfde39d3a2cc9786820672c7f
allenchan95 <35793136+allenchan95@users.noreply.github.com>
2019-01-07 19:46:47 +0800

    update search feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
-const App = () => {
-	return (
+
+
+
+
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+	
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value});
+
+	}
+	render() {
+		const filterRobots = this.state.robots.filter(robots =>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+
+		console.log(filterRobots);
+		return (
 		<div className='tc'>
 			<h1>RobotFriends</h1>	
-			<SearchBox />
-     		<CardList robots={robots}/>
+			<SearchBox searchChange={this.onSearchChange} />
+     		<CardList robots={filterRobots}/>
      	</div>
-	);
+		);
+	}
+	
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = ({name,email,id}) =>{
 	return(
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shawdow-5'>
-			<img alt='robots' src={`https://robohash.org/${id}?200*200`}/>
+		<div className='tc bg-lightest-blue dib br4 pa3 ma2 grow bw2 shawdow-3'>
+			<img alt='robots' src={`https://robohash.org/${id}`}/>
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
