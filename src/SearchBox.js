ae1639d096f9ad2153a593a62a6c3d84560f7077
allenchan95 <35793136+allenchan95@users.noreply.github.com>
2019-01-07 15:12:29 +0800

    robot friend -react App

---
+++ src/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchBox = () => {
+	return (
+			<div className='pa2'>
+				<input
+				 className='pa3 ba b--green bg-light-blue'
+				 type='search' 
+				 placeholder='search robot'
+				 />
+			 </div>
+
+		);
+	
+}
+
+export default SearchBox;
